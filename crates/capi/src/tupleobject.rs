dddbe466339bae322dbee0325ab18f68ef6cb753
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-06-15 23:44:20 +0200

    Allow calling `PyTuple_SetItem`

--- crates/capi/src/tupleobject.rs
+++ crates/capi/src/tupleobject.rs
@@ -2,10 +2,11 @@ use crate::PyObject;
 use crate::object::define_py_check;
 use crate::pystate::with_vm;
 use core::ffi::c_int;
+use core::ptr::{self, NonNull};
 use core::slice;
-use rustpython_vm::PyResult;
 use rustpython_vm::builtins::PyTuple;
 use rustpython_vm::sliceable::SliceableSequenceOp;
+use rustpython_vm::{AsObject, PyObjectRef, PyResult};
 
 define_py_check!(fn PyTuple_Check, types.tuple_type);
 define_py_check!(exact fn PyTuple_CheckExact, types.tuple_type);
@@ -14,12 +15,13 @@ define_py_check!(exact fn PyTuple_CheckExact, types.tuple_type);
 pub extern "C" fn PyTuple_New(len: isize) -> *mut PyObject {
     with_vm(|vm| {
         if len == 0 {
-            return Ok(vm.ctx.empty_tuple.to_owned());
+            Ok(vm.ctx.empty_tuple.to_owned())
+        } else {
+            let len: usize = len
+                .try_into()
+                .map_err(|_| vm.new_system_error("Negative size passed to PyTuple_New"))?;
+            Ok(vm.new_tuple(vec![vm.ctx.none(); len]))
         }
-
-        Err(vm.new_not_implemented_error(
-            "PyTuple_New for non zero sized tuples is not supported, use PyTuple_FromArray instead",
-        ))
     })
 }
 
@@ -42,14 +44,30 @@ pub unsafe extern "C" fn PyTuple_FromArray(
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn PyTuple_SetItem(
-    _tuple: *mut PyObject,
-    _pos: isize,
-    _value: *mut PyObject,
+pub unsafe extern "C" fn PyTuple_SetItem(
+    tuple: *mut PyObject,
+    pos: isize,
+    value: *mut PyObject,
 ) -> c_int {
-    with_vm::<PyResult<()>, _>(
-        |vm| Err(vm.new_not_implemented_error("Tuple objects are immutable")),
-    )
+    with_vm::<PyResult<()>, _>(|vm| {
+        let tuple = unsafe { &*tuple }.try_downcast_ref::<PyTuple>(vm)?;
+        let index: usize = pos
+            .try_into()
+            .map_err(|_| vm.new_system_error("negative position"))?;
+
+        if tuple.len() <= index {
+            Err(vm.new_index_error("tuple index out of range"))
+        } else if tuple.as_object().strong_count() > 1 {
+            Err(vm.new_system_error("Tuple objects are immutable when the reference count > 1"))
+        } else {
+            // SAFETY: We have exclusive access to the tuple payload (strong_count == 1),
+            // so mutating the selected element in place is valid.
+            let data_ptr = unsafe { tuple.as_ptr().add(index) as *mut PyObjectRef };
+            let value = unsafe { PyObjectRef::from_raw(NonNull::new_unchecked(value)) };
+            unsafe { ptr::replace(data_ptr, value) };
+            Ok(())
+        }
+    })
 }
 
 #[unsafe(no_mangle)]
@@ -92,6 +110,7 @@ pub unsafe extern "C" fn PyTuple_GetSlice(
 
 #[cfg(false)]
 mod tests {
+    use pyo3::ffi;
     use pyo3::prelude::*;
     use pyo3::types::PyTuple;
 
@@ -117,6 +136,31 @@ mod tests {
             let tuple = (1, 2, 3).into_pyobject(py).unwrap();
             let slice = tuple.get_slice(1, 2);
             assert_eq!(slice.extract::<(u32,)>().unwrap(), (2,));
+        })
+    }
+
+    #[test]
+    fn tuple_is_mutable_with_one_ref() {
+        Python::attach(|py| {
+            let value: Bound<'_, _> = 1.into_pyobject(py).unwrap();
+            let tuple: Bound<'_, PyTuple> =
+                unsafe { Bound::from_owned_ptr(py, ffi::PyTuple_New(1)).cast_into_unchecked() };
+            assert!(tuple.get_item(0).unwrap().is_none());
+            unsafe {
+                assert_eq!(
+                    ffi::PyTuple_SetItem(tuple.as_ptr(), 0, value.clone().into_ptr()),
+                    0
+                );
+            }
+            assert_eq!(tuple.get_item(0).unwrap().extract::<u32>().unwrap(), 1);
+            let _new_ref = tuple.clone();
+            unsafe {
+                assert_eq!(
+                    ffi::PyTuple_SetItem(tuple.as_ptr(), 0, value.clone().into_ptr()),
+                    -1
+                );
+            }
+            assert!(PyErr::take(py).is_some())
         })
     }
 }
